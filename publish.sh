64dd11464429c18c6883aee88c8ecd736f0d7bab
Violet Nēšana <violet@nesana.org>
2020-10-12 14:27:35 -0700

    Rename project to TransKit.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-poetry run jupyter nbconvert Estradiol.ipynb Cycles.ipynb Injectables.ipynb --to html --template lab --output-dir ../hormone-pages
+poetry run jupyter nbconvert Estradiol.ipynb Cycles.ipynb Injectables.ipynb --to html --template lab --output-dir ../transkit-pages
